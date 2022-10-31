e71a19ff6f625f08de082f7f85788858c4692007
Fernando Galvan <n.brighter00@gmail.com>
2022-10-31 18:47:08 -0500

    added rest api for reviews

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from crypt import methods
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -17,10 +18,65 @@ class Review(db.Model):
     def __init__(self, description):
         self.description = description
 
+def format_review(review):
+    return {
+        "description": review.description,
+        "id": review.id,
+        "created_at": review.created_at
+    }
 
+#index route
 @app.route('/')
 def hello():
     return "hey"
+
+
+#create route
+@app.route('/review', methods = ['POST'])
+def create__review():
+    description = request.json['description']
+    review = Review(description)
+    db.session.add(review)
+    db.session.commit()
+    return format_review(review)
+
+#read (get all)
+@app.route('/review', methods= ['GET'])
+def get_reviews():
+    reviews = Review.query.order_by(Review.id.asc()).all()
+    review_list = []
+    for review in reviews:
+        review_list.append(format_review(review))
+    return{'reviews': review_list}
+
+#read (single)
+@app.route('/review/<id>', methods=['GET'])
+def get_review(id):
+    review = Review.query.filter_by(id=id).one()
+    formatted_review = format_review(review)
+    return{'review': formatted_review}
+
+
+#update route
+@app.route('/review/<id>', methods= ['PUT'])
+def update_review(id):
+    review = Review.query.filter_by(id=id)
+    description = request.json['description']
+    review.update(dict(description = description, created_at = datetime.utcnow()))
+    db.session.commit()
+    return {"review": format_review(review.one())}
+
+
+#delete route
+@app.route('/review/<id>', methods = ['DELETE'])
+def delete_review(id):
+    review = Review.query.filter_by(id=id).one()
+    db.session.delete(review)
+    db.session.commit()
+    return 'Review Deleted'
+
+
+
 
 if __name__ == '__main__':
     app.run()
